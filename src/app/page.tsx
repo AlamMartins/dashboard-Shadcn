2c630a21bfa533c2ebe71f5aae1e9083863a5631
AlamMartins <alammartins@haf.com.br>
2024-08-14 07:38:22 -0300

    upload first step project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {Button} from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <div className="">
+      Dashboard AM Solutions
+    </div>
+  )
+}
+
+export default page
